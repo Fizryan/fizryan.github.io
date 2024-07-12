1502c10dc03166f665878cfd165e326d1f3ff84a
Fizryan <tdark9362@gmail.com>
2024-07-12 18:21:27 +0700

    New Version

---
+++ javascript.js
@@ -0,0 +1,31 @@
+function redirInstagram() {
+    window.location.href = "https://www.instagram.com/_fizryan/";
+}
+
+function redirGithhub() {
+    window.location.href = "https://github.com/Fizryan";
+}
+
+function redirDiscord() {
+    window.location.href = "https://discord.gg/6qSdavg8uJ";
+}
+
+function redirSpotify() {
+    window.location.href = "https://open.spotify.com/user/31sacpnete6pic6apfx3g2r2wcxe";
+}
+
+function redirSteam() {
+    window.location.href = "https://steamcommunity.com/profiles/76561198305039331/";
+}
+
+function redirTwitch() {
+    window.location.href = "https://www.twitch.tv/zarfana";
+}
+
+function redirEmail() {
+    window.location.href = "mailto:hafizryandin@outlook.com";
+}
+
+function redirProfile() {
+    window.location.href = "profile.html";
+}
